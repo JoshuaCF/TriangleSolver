b46893b9c33a3945b4beaab56e809b8d9dfbc060
Joshua <joshcade789@gmail.com>
2017-09-21 15:16:30 -0500

    Update Triangle.java

--- src/main/Triangle.java
+++ src/main/Triangle.java
@@ -1,19 +1,28 @@
-package myprojects.trianglecalc;
+package trianglecalc;
 
 import java.util.*;
 
 public class Triangle {
 	//Properties
-	Double a;
-	Double b;
-	Double c;
-	Double h;
-	Double area;
-	Double angleAb;
-	Double angleAc;
+	public Double a; //Base
+	public Double b;
+	public Double c;
+	public Double h;
+	public Double area;
 	//Constructor
 	public Triangle(Double a, Double b, Double c) {
-		
+		this.a = a;
+		this.b = b;
+		this.c = c;
+		setArea();
+		setHeight();
 	}
 	//Methods
+	private void setArea() {
+		Double s = (this.a + this.b + this.c)/2;
+		this.area = Math.sqrt(s * (s - this.a) * (s - this.b) * (s - this.c));
+	}
+	private void setHeight() {
+		this.h = 2 * (this.area / this.a);
+	}
 }
